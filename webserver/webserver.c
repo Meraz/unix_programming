3204afe5793ccc4e9f73de5c5d42c17ba3987bf7
Jesper Liljeblad <jesper.liljeblad@gmail.com>
2014-10-29 22:55:32 +0100

    Added extension check and a function for creating 200-header.

--- webserver/webserver.c
+++ webserver/webserver.c
@@ -11,6 +11,12 @@
 #define BUFSIZE 4096
 #define MAX_QUEUE 20
 
+struct mime_types
+{
+	char content_type[80];
+	char ending[7][10];
+};
+
 void read_config_file(int *port, char *wsroot);
 void check_ws_root(char *wsroot);
 void parse_arguments(int argc, char **argv, int *port, int *daemon, char *log_file);
@@ -18,6 +24,9 @@ void start_server(struct sockaddr_in address, int *port, int *listener);
 void wait_for_connection(struct sockaddr_in address, int *listener);
 void handle_request(int new_socket);
 char *resolve_path(char *uri);
+void create_ok_header(char *uri, char *buffer);
+char *get_extension(char *path);
+void *get_content_type(char *extension, char *content_type);
 
 int main(int argc, char* argv[])
 {
@@ -60,7 +69,6 @@ void read_config_file(int *port, char *wsroot)
 
 	if(config_file == NULL)
 	{
-		//TODO Log
 		exit(1);
 	}
 
@@ -185,7 +193,7 @@ void handle_request(int new_socket)
 	uri = resolve_path(strsep(&temp, " \r\n")); //Path
 	httpv = strsep(&temp, " \r\n"); //HTTP version
 
-	//If it is not HTTP/1.0 or HTTP/1.1, send Bad Request
+	//If it is not HTTP/1.0 or HTTP/1.1, 400
 	if ((strcmp(httpv, "HTTP/1.0") != 0 && strcmp(httpv, "HTTP/1.1") != 0))
 	{
 		strcpy(buffer, "HTTP/1.0 400 Bad Request\r\n");
@@ -193,72 +201,70 @@ void handle_request(int new_socket)
 		goto closing_down;
 	}
 
+	resolved = realpath(uri, NULL);
+	if(resolved == NULL)
+	{
+		//File does not exist, 404
+		strcpy(buffer, "HTTP/1.0 404 Not Found\r\n");
+		send(new_socket, buffer, strlen(buffer), 0);
+		goto closing_down;
+	}
+
 	//GET requests
 	if(strcmp(type, "GET") == 0)
 	{
-		//REALPATH hates ALL teh paths! <3
-		resolved = realpath(uri, NULL);
-		if(resolved == NULL)
-		{
-			strcpy(buffer, "HTTP/1.0 403 Forbidden\r\n");
-			send(new_socket, buffer, strlen(buffer), 0);
-			goto closing_down;
-		}
-		
-		//Check if the requested file exists
+		//Opening requested file
 		if((openfile = open(uri, O_RDONLY)) != -1)
 		{
-			//It does, 200
-			strcpy(buffer, "HTTP/1.0 200 OK\r\n\r\n");
+			//File opens, 200
+			create_ok_header(uri, buffer);
 			send(new_socket, buffer, strlen(buffer), 0);
-			sendfile(new_socket, openfile, NULL, /*filesize*/244);
+			if(sendfile(new_socket, openfile, NULL, /*filesize*/244) == -1)
+			{
+				//Error sending file, 500
+				strcpy(buffer,"HTTP/1.0 500 Internal Server Error\r\n");
+				send(new_socket, buffer, strlen(buffer), 0);
+			}
 		}
-		//It does not exist, 404
 		else
 		{
-			strcpy(buffer, "HTTP/1.0 404 Not Found\r\n");
+			//Can't open file, 403
+			strcpy(buffer, "HTTP/1.0 403 Forbidden\r\n");
 			send(new_socket, buffer, strlen(buffer), 0);
 		}		
 	}
 	//HEAD requests
 	else if(strcmp(type, "HEAD") == 0)
 	{
-		resolved = realpath(uri, NULL);
-		if(resolved == NULL)
-		{
-			strcpy(buffer, "HTTP/1.0 403 Forbidden\r\n");
-			send(new_socket, buffer, strlen(buffer), 0);
-			goto closing_down;
-		}
-		
-		//Checking if the file exists
+		//Opening requested file
 		if((openfile = open(uri, O_RDONLY)) != -1)
 		{
-			//File found, return 200
-			strcpy(buffer, "HTTP/1.0 200 OK\r\n");
+			//File opens, 200
+			create_ok_header(uri, buffer);
 			send(new_socket, buffer, strlen(buffer), 0);
 		}
 		else
 		{
-			//File not found, return 404
-			strcpy(buffer,"HTTP/1.0 404 Not Found\r\n");
+			//Can't open file, 403
+			strcpy(buffer, "HTTP/1.0 403 Forbidden\r\n");
 			send(new_socket, buffer, strlen(buffer), 0);
 		}
 	}
-	//Not implemented requests
+	//All other requests
 	else
 	{
+		//Not implemented, 501
 		strcpy(buffer, "HTTP/1.0 501 Not Implemented\r\n");
 		send(new_socket,buffer,strlen(buffer),0);
 	}
 	closing_down:
+	free(resolved);
 	close(openfile);
 	close(new_socket);
 }
 
 char *resolve_path(char *uri)
 {
-	printf("PATH: %s\n", uri);
 	if(strncmp(uri, "/\0", 2) == 0)
 	{
 		return "index.html";
@@ -266,5 +272,59 @@ char *resolve_path(char *uri)
 	else if(strncmp(uri, "/", 1) == 0)
 	{
 		return uri + 1;
+	}
+}
+
+void create_ok_header(char *uri, char *buffer)
+{
+	char content_type[32];
+	char *file_extension;
+	file_extension = get_extension(uri);
+	get_content_type(file_extension, content_type);
+	strcpy(buffer,"HTTP/1.0 200 OK\r\nContent-Type: ");
+	strcat(buffer, content_type);
+	strcat(buffer, "\r\n\r\n");
+}
+
+char *get_extension(char *path)
+{
+	char *dot = strrchr(path, '.');
+    if(!dot || dot == path)
+	{
+		return "";
+	}
+    return dot + 1;
+}
+
+void *get_content_type(char *extension, char *content_type)
+{
+	FILE *extension_file = fopen("../supported.extensions", "r");
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	if(extension_file == NULL)
+	{
+		exit(1);
+	}
+
+	while((read = getline(&line, &len, extension_file)) != -1)
+	{
+		if(line[0] == '#')
+		{
+			continue;
+		}
+
+		if(strncmp(line, extension, strlen(extension)) == 0) //Extension found
+		{
+			sscanf(line, "%*s %s", content_type); //Get content-type
+			break;
+		}
+	}
+
+	fclose(extension_file);
+	if(line)
+	{
+		free(line);
 	}
 }
