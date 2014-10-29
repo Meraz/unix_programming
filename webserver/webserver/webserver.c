db7e2c63465dc14d633fbdf8d69673fb1121893c
Jesper Liljeblad <jesper.liljeblad@gmail.com>
2014-10-29 15:22:20 +0100

    Added a bunch of stuff! REALPATHES hates us all. Chroot does not want to play... Cleaned up the wsroot function.

--- webserver/webserver/webserver.c
+++ webserver/webserver/webserver.c
@@ -2,40 +2,53 @@
 #include<stdlib.h>
 #include<string.h>
 #include<fcntl.h>
-#include<time.h>
 #include<syslog.h>
 #include<unistd.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<sys/mman.h>
 
+#include<errno.h>
+
+#define BUFSIZE 4096
 #define MAX_QUEUE 20
 
 void read_config_file(int *port, char *wsroot);
-void set_ws_root_directory(char *path, char *wsroot);
+void check_ws_root(char *wsroot);
 void parse_arguments(int argc, char **argv, int *port, int *daemon, char *log_file);
-void start_server(int *port);
+void start_server(struct sockaddr_in address, int *port, int *listener);
+void wait_for_connection(struct sockaddr_in address, int *listener);
+void handle_request(int new_socket);
 
 int main(int argc, char* argv[])
 {
-	char ws_root_path[256];
+	struct sockaddr_in address;
 	char wsroot[32];
 	int port;
 	int daemon = 0;
 	char log_file[32] = "webserv.log";
+	int listener;
 
 	//Read values from config
 	read_config_file(&port, wsroot);
-	//Setting root directory
-	set_ws_root_directory(ws_root_path, wsroot);
+	//Creates preferred root dir, if it does not exist
+	check_ws_root(wsroot);
 	//Parsing arguments
 	parse_arguments(argc, argv, &port, &daemon, log_file);
+	
+	//Set current dir
+	//TODO, fix this + 1 crap...
+	chdir(wsroot + 1);
+	//TODO Set chroot...
 
-	/* TODO Add chroot check
-	Add daemon */
+	// TODO Add daemon stuff
 
 	//Start server
-	start_server(&port);
+	start_server(address, &port, &listener);
+	//Wait for connections
+	wait_for_connection(address, &listener);
+	//Close listener socket
+	close(listener);
 
 	return 0;
 }
@@ -77,18 +90,14 @@ void read_config_file(int *port, char *wsroot)
 	}
 }
 
-void set_ws_root_directory(char *path, char *wsroot)
+void check_ws_root(char *wsroot)
 {
 	char *executing_directory;
 	struct stat l_stat = {0};
-	if (stat(wsroot + 1, &l_stat) == -1) 	// if the folder does not exist, create it
+	if (stat(wsroot + 1, &l_stat) == -1) // if the folder does not exist, create it
 	{
 		mkdir(wsroot + 1, 0700);
 	}
-
-	executing_directory = getenv("PWD");
-	strcpy(path, executing_directory);		// Set root directory to executing_directory
-	strcat(path, wsroot);				// Append root folder to the current directory 
 }
 
 void parse_arguments(int argc, char **argv, int *port, int *daemon, char *log_file)
@@ -111,20 +120,118 @@ void parse_arguments(int argc, char **argv, int *port, int *daemon, char *log_fi
 	}
 }
 
-void start_server(int *port)
+void start_server(struct sockaddr_in address, int *port, int *listener)
 {
-	struct sockaddr_in address;
-	int listener;
 	int opt = 1;
 
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(*port);
 	
-	listener = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	*listener = socket(AF_INET, SOCK_STREAM, 0);
+	setsockopt(*listener, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-	bind(listener, (struct sockaddr *) &address, sizeof(address));
+	bind(*listener, (struct sockaddr *) &address, sizeof(address));
+	
+	listen(*listener, MAX_QUEUE);
+}
 
-	listen(listener, MAX_QUEUE);
+void wait_for_connection(struct sockaddr_in address, int *listener)
+{
+	int new_socket;
+	pid_t pid;
+
+	int addrlen = sizeof(address);
+	while(1)
+	{
+		new_socket = accept(*listener, (struct sockaddr*) &address, &addrlen);
+		
+		if((pid = fork()) < 0)
+		{
+			//Forking failed
+			close(new_socket);
+		}
+		else if(pid == 0)
+		{
+			// This is the child process
+			//Close parent socket and handle the request
+			close(*listener);
+			handle_request(new_socket);
+			close(new_socket);
+			exit(0);
+		}
+		else
+		{
+			// This is the parent process
+			// Kill child process and close child socket
+			//TODO Don't wait, just keep accepting new connections.
+			wait(NULL);
+			close(new_socket);
+		}
+	}
+}
+
+void handle_request(int new_socket)
+{
+	char buffer[BUFSIZE];
+	char *temp = NULL;
+	char *uri = NULL;
+	char *httpv = NULL;
+	char *resolved = NULL;
+	int openfile;
+
+	recv(new_socket, buffer, BUFSIZE, 0);
+
+	if(strncmp(buffer, "GET", 3) == 0)
+	{
+		temp = buffer;
+		uri = strsep(&temp, " \r\n"); //First part is "GET", we want the path
+		uri = strsep(&temp, " \r\n"); //Path
+		httpv = strsep(&temp, " \r\n"); //HTTP version
+
+		//If it is not HTTP/1.0 or HTTP/1.1, send Bad Request
+		if ((strcmp(httpv, "HTTP/1.0") != 0 && strcmp(httpv, "HTTP/1.1") != 0))
+		{
+			strcpy(buffer, "HTTP/1.0 400 Bad Request\r\n");
+			send(new_socket, buffer, strlen(buffer), 0);
+		}
+		
+		//TODO REALPATH hates ALL teh paths! <3
+		/*resolved = realpath(uri, NULL);
+		printf("uri: %s REALPATH: %s\n", uri, resolved);*/
+		/*if(resolved == NULL)
+		{
+			strcpy(buffer, "HTTP/1.0 403 Forbidden\r\n");
+			send(new_socket, buffer, strlen(buffer), 0);
+		}*/
+
+		if(strcmp(uri, "/") == 0)
+		{
+			uri = "index.html";
+		}
+		
+		//Check if the requested file exists
+		//It does, 200
+		if((openfile = open(uri, O_RDONLY)) != -1)
+		{
+			strcpy(buffer, "HTTP/1.0 200 OK\r\n\r\n");
+			send(new_socket, buffer, strlen(buffer), 0);
+			sendfile(new_socket, openfile, NULL, /*filesize*/244);
+		}
+		//It does not exist, 404
+		else
+		{
+			strcpy(buffer, "HTTP/1.0 404 Not Found\r\n");
+			send(new_socket, buffer, strlen(buffer), 0);
+		}		
+	}
+	else if(strncmp(buffer, "HEAD", 4) == 0)
+	{
+		printf("HEAD request...\n");
+	}
+	else
+	{
+		strcpy(buffer, "HTTP/1.0 501 Not Implemented\r\n");
+		send(new_socket,buffer,strlen(buffer),0);
+	}
 }
