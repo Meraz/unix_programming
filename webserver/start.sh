0455b176857066a09318d78c28947e70374053d6
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2014-10-30 17:35:44 +0100

    New start script works gloriously perfect! d:

--- webserver/start.sh
+++ webserver/start.sh
@@ -1,5 +1,58 @@
 #! /bin/bash
 clear
+
+# Function flags
+pFlag=0
+lFlag=""
+dFlag=0
+
+# Out
+out='./wserver'
+
+ArgumentUse() {
+	echo "This is not how you use this file!"
+}
+
+# Check and set flags
+while getopts p:l:d option
+do
+	case $option in
+	p)
+#		echo "-n was triggered, Paramenter: $OPTARG"
+		pFlag=$OPTARG ;;
+	l)
+#		echo "-h was triggered, Paramenter: $OPTARG"
+		lFlag=$OPTARG;;
+	d)
+#		echo "-d was triggered"
+		dFlag=1;;
+	\?)
+		ArgumentUse; exit 0;;
+	*)
+		ArgumentUse; exit 1;;
+	esac
+done
+
+
+if [ ! $pFlag -eq 0 ] ; 
+	then
+	out="$out -p $pFlag"
+	echo $out;
+fi
+
+
+if [ ! -z "$lFlag"  ] ; 
+	then
+	out="$out -l $lFlag"
+	echo $out;
+fi
+
+if [ ! $dFlag -eq 0 ] ; 
+	then
+	out="$out -d"
+	echo $out;
+fi
+
 id=`ps aux | grep wserver | grep -v grep | cut -d " "  -f4`
 
 if ! [ -z "$id" ]; then
@@ -7,4 +60,5 @@ if ! [ -z "$id" ]; then
 	kill $id
 fi
 
-./wserver -p $1
+#echo $out
+`$out`
